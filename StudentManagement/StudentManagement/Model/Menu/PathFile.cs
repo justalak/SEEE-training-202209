53f64d32b951423f6b02ebecfb4f301f814a959c
HOANG THE MANH <77549345+hoangthemanhzin@users.noreply.github.com>
2022-09-28 23:48:25 +0700

    Update PathFile.cs

--- StudentManagement/StudentManagement/Model/Menu/PathFile.cs
+++ StudentManagement/StudentManagement/Model/Menu/PathFile.cs
@@ -8,8 +8,8 @@ namespace StudentManagement.Model.Menu
 {
     internal class PathFile
     {
+        //Mọi người đưa code về máy thì sửa path của các file json về máy để chạy ạ
         public string StudentPath = @"D:\TAILIEUHOANGTHEMANH\AvePointTraining\StudentManagement\StudentManagement\Model\JsonFile\student.json";
-        
         public string TeacherPath = @"D:\TAILIEUHOANGTHEMANH\AvePointTraining\StudentManagement\StudentManagement\Model\JsonFile\teacher.json";
         public string ClassPath = @"D:\TAILIEUHOANGTHEMANH\AvePointTraining\StudentManagement\StudentManagement\Model\JsonFile\class.json";
     }
